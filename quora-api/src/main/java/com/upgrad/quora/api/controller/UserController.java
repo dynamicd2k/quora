83affdb0be7ef62fb5166617bae35100a8be9d46
Abhishek Aman <abhishek.aman@trakinvest.com>
2019-10-28 18:17:13 +0530

    added signUp API

--- quora-api/src/main/java/com/upgrad/quora/api/controller/UserController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/UserController.java
@@ -1,28 +1,28 @@
 package com.upgrad.quora.api.controller;
 
 import com.upgrad.quora.api.model.SigninResponse;
+import com.upgrad.quora.api.model.SignoutResponse;
 import com.upgrad.quora.api.model.SignupUserRequest;
 import com.upgrad.quora.api.model.SignupUserResponse;
 import com.upgrad.quora.service.business.SigninService;
+import com.upgrad.quora.service.business.SignoutService;
 import com.upgrad.quora.service.business.SignupService;
 import com.upgrad.quora.service.business.UserService;
 import com.upgrad.quora.service.exception.AuthenticationFailedException;
+import com.upgrad.quora.service.exception.SignOutRestrictedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.upgrad.quora.service.entity.*;
 
 import java.util.Base64;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired
@@ -34,7 +34,10 @@ public class UserController {
     @Autowired
     private SigninService signinService;
 
-    @RequestMapping(method= RequestMethod.POST, value = "/user/signup")
+    @Autowired
+    private SignoutService signoutService;
+
+    @PostMapping("/signup")
     public ResponseEntity<SignupUserResponse> signup(final SignupUserRequest signupUserRequest) throws Exception {
 
         final UserEntity user = new UserEntity();
@@ -56,7 +59,7 @@ public class UserController {
         return new ResponseEntity(HttpStatus.OK);
     }
 
-    @RequestMapping(method = RequestMethod.POST,path = "/user/signin", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @PostMapping(path = "/signin", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<SigninResponse> signin(@RequestHeader("authorization") final String authorization) throws AuthenticationFailedException {
 
         //decoding the username and password from the authorization RequestHeader
@@ -74,5 +77,14 @@ public class UserController {
         return new ResponseEntity<SigninResponse>(signinResponse,httpHeaders, HttpStatus.OK);
     }
 
+    @PostMapping(path = "/signout", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<SignoutResponse> signout(@RequestHeader("authorization") final String authorization) throws SignOutRestrictedException {
+
+        UserAuthEntity token = signoutService.signout(authorization);
+        UserEntity user = token.getUser();
+        SignoutResponse signoutResponse=new SignoutResponse().id(user.getUuid()).message("SIGNED OUT SUCCESSFULLY");
+
+        return new ResponseEntity<SignoutResponse>(signoutResponse,HttpStatus.OK);
+    }
 
 }
