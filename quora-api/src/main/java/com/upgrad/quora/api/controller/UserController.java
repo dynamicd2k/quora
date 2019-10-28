f36eb42d6824b3de84c41d9681bed5c274649d36
Abhishek Aman <abhishek.aman@trakinvest.com>
2019-10-28 18:05:50 +0530

    added signIn API

--- quora-api/src/main/java/com/upgrad/quora/api/controller/UserController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/UserController.java
@@ -1,17 +1,24 @@
 package com.upgrad.quora.api.controller;
 
+import com.upgrad.quora.api.model.SigninResponse;
 import com.upgrad.quora.api.model.SignupUserRequest;
 import com.upgrad.quora.api.model.SignupUserResponse;
+import com.upgrad.quora.service.business.SigninService;
 import com.upgrad.quora.service.business.SignupService;
 import com.upgrad.quora.service.business.UserService;
+import com.upgrad.quora.service.exception.AuthenticationFailedException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import com.upgrad.quora.service.entity.*;
 
+import java.util.Base64;
 import java.util.UUID;
 
 @RestController
@@ -23,6 +30,9 @@ public class UserController {
 
     @Autowired
     private SignupService signupBusinessService;
+
+    @Autowired
+    private SigninService signinService;
 
     @RequestMapping(method= RequestMethod.POST, value = "/user/signup")
     public ResponseEntity<SignupUserResponse> signup(final SignupUserRequest signupUserRequest) throws Exception {
@@ -45,5 +55,24 @@ public class UserController {
         SignupUserResponse userResponse = new SignupUserResponse().id(createdUserEntity.getUuid()).status("USER SUCCESSFULLY REGISTERED");
         return new ResponseEntity(HttpStatus.OK);
     }
+
+    @RequestMapping(method = RequestMethod.POST,path = "/user/signin", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<SigninResponse> signin(@RequestHeader("authorization") final String authorization) throws AuthenticationFailedException {
+
+        //decoding the username and password from the authorization RequestHeader
+        byte[] decode = Base64.getDecoder().decode(authorization);
+        String decodedText = new String(decode);
+        String[] decodedArray = decodedText.split(":");
+
+        //passing the username and password to the signupBusinessService
+        UserAuthEntity token = this.signinService.login(decodedArray[0], decodedArray[1]);
+        UserEntity user = token.getUser();
+
+        SigninResponse signinResponse=new SigninResponse().id((user.getUuid())).message("SIGNED IN SUCCESSFULLY");
+        HttpHeaders httpHeaders=new HttpHeaders();
+        httpHeaders.add("access-token", token.getAccessToken());
+        return new ResponseEntity<SigninResponse>(signinResponse,httpHeaders, HttpStatus.OK);
+    }
+
 
 }
---
+++ quora-service/src/main/java/com/upgrad/quora/service/business/SigninService.java
@@ -0,0 +1,53 @@
+package com.upgrad.quora.service.business;
+
+import com.upgrad.quora.service.dao.UserDao;
+import com.upgrad.quora.service.entity.UserAuthEntity;
+import com.upgrad.quora.service.entity.UserEntity;
+import com.upgrad.quora.service.exception.AuthenticationFailedException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
+@Service
+public class SigninService {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Autowired
+    private PasswordCryptographyProvider passwordCryptographyProvider;
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public UserAuthEntity login(final String username, final String password) throws AuthenticationFailedException {
+
+        UserEntity user = userDao.getUserByUsername(username);
+        //if the username is invalid AuthenticationFailedException is thrown
+        if (user == null) {
+            throw new AuthenticationFailedException("ATH-001","This username does not exist");
+        }
+        final String encryptedPassword = passwordCryptographyProvider.encrypt(password,user.getSalt());
+        //if password matches then authToken is created for the user and registered in the database
+        if(encryptedPassword.equals(user.getPassword())) {
+
+            UserAuthEntity userAuthToken = new UserAuthEntity();
+            JwtTokenProvider jwtTokenProvider=new JwtTokenProvider(encryptedPassword);
+            userAuthToken.setUser(user);
+            userAuthToken.setUuid(UUID.randomUUID().toString());
+            final ZonedDateTime now = ZonedDateTime.now();
+            final ZonedDateTime expiresAt = now.plusHours(8);
+            userAuthToken.setAccessToken(jwtTokenProvider.generateToken(user.getUuid(),now,expiresAt));
+            userAuthToken.setLoginAt(now);
+            userAuthToken.setExpiresAt(expiresAt);
+            userDao.createAuthToken(userAuthToken);
+            return userAuthToken;
+        }
+        //if password does not match, AuthenticationFailedException is thrown
+        else {
+            throw new AuthenticationFailedException("ATH-002", "Password failed");
+        }
+    }
+}
